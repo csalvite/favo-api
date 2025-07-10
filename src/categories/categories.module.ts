6060db11739fdab08ae5bb4ab765305dc71f025c
César Alvite <csalvite@gmail.com>
2025-07-10 19:27:53 +0200

    return all categories

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ResponseModule } from 'src/common/responses/response.module';
 
 @Module({
+  imports: [PrismaModule, ResponseModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,15 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ResponseService } from 'src/common/responses/response.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly responseService: ResponseService,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll() {
+    try {
+      const categories = await this.prisma.category.findMany();
+
+      if (!categories || categories.length === 0) {
+        throw new NotFoundException('Categories not found.');
+      }
+
+      return this.responseService.success(
+        'Categories retrieved successfully',
+        categories,
+      );
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException('Categories not found.');
+        }
+      }
+      throw new InternalServerErrorException('Unexpected error.');
+    }
   }
 
   findOne(id: number) {
